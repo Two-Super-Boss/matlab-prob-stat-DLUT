143190a15fb6af021d645a4c3a0fd75719463f57
qhy040404 <45379733+qhy040404@users.noreply.github.com>
2022-10-19 17:41:04 +0800

    Update b4.m

--- 概率上机/b4.m
+++ 概率上机/b4.m
@@ -1,11 +1,11 @@
 X1=randn(1000,1);
 X2=randn(1000,1);
 Y=X1.^2+X2.^2;
-max_data=max(Y);
-min_data=min(Y);
-bins=100;
-bin_space=(max_data-min_data)/bins;
+max_data=max(Y); % Max
+min_data=min(Y); % Min
+bins=100; % Divide into 100 parts
+bin_space=(max_data-min_data)/bins; % length per bin
 xtick=min_data:bin_space:max_data-bin_space;
-dist=hist(Y,bins);
-pdf=(bins*dist)/(sum(dist)*(max_data-min_data));
+dist=hist(Y,bins); % Use histograms to get total count in each part
+pdf=(bins*dist)/(sum(dist)*(max_data-min_data)); % figure pdf
 plot(xtick,pdf)
